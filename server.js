63cecadcfd2aaa23cb31ff3aaad3e87e69ad9832
luis206r <luisr_02@outlook.es>
2024-05-21 13:01:49 -0500

    changed domain

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const envs = require("./src/config/envs");
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
-app.use(cors({ origin: "https://student-sync-pi.vercel.app", credentials: true })); // para comunicar entre puertos
+app.use(cors({ origin: "https://student-collab.vercel.app", credentials: true })); // para comunicar entre puertos
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
