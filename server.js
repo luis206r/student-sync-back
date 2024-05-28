aba69f3118d1e1d6593aad68b286a9632da977ae
Luis Robledo <70814892+luis206r@users.noreply.github.com>
2024-05-28 17:22:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api", routes);
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
 
-db.sync({ force: true })
+db.sync({ force: false })
   .then(() => {
     app.listen(8000, () => {
       console.log("Escuchando en el puerto 8000");
